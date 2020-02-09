53c5f6276a1d82cf2aba1c874210e53def08f87e
Lloyd Kuijs <42713555+lloydkuijs@users.noreply.github.com>
2020-02-09 11:29:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,16 +11,8 @@ setuptools.setup(
     description="A simple python library showing off how easy it is to setup a voice based assistant with the Rasa framework or any or HTTP server that takes string requests",
     url="https://github.com/lloydkuijs/AConverse",
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Development Status :: 3 - Alpha"
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: POSIX :: Linux",
-
-    ],
     python_requires='>=3.5',
     install_requires=[
-        'rasa>=1.6.0',
         'SpeechRecognition>=3.8.1',
         'pydub>=0.23.1',
         'requests>=2.22.0',
